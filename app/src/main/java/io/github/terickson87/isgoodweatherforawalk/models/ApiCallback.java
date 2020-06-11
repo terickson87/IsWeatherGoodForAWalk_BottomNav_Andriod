a2df4e76c0e5b04c67b6be0acce2592b7fae79ed
Todd Erickson <twericks@usc.edu>
2020-06-11 12:08:56 -0700

    Added various models to the app.

---
+++ app/src/main/java/io/github/terickson87/isgoodweatherforawalk/models/ApiCallback.java
@@ -0,0 +1,11 @@
+package io.github.terickson87.isgoodweatherforawalk.models;
+
+import org.json.JSONObject;
+
+
+/**
+ * The ApiCallback interface is a generic callback interface intended to be used with REST Apis that return a JSONObject response.
+ */
+public interface ApiCallback {
+    void onSuccess(JSONObject response);
+}
