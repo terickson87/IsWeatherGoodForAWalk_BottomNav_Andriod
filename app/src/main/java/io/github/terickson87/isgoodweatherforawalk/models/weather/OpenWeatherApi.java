28aa20e96fa1895c35eaec261bded097e9f2bf69
Todd Erickson <twericks@usc.edu>
2020-06-14 18:31:02 -0700

    Made the JSON fields be tagged with variables instead of raw strings.

--- app/src/main/java/io/github/terickson87/isgoodweatherforawalk/models/weather/OpenWeatherApi.java
+++ app/src/main/java/io/github/terickson87/isgoodweatherforawalk/models/weather/OpenWeatherApi.java
@@ -34,16 +34,48 @@ import java.util.List;
  */
 public class OpenWeatherApi {
 
-    // Static Props
-    private static final String TAG = "OpenWeatherApi";
-    private static final String mc_API_KEY =  "***REMOVED***";
-    private static final String mc_ONE_CALL_API_URL = "https://api.openweathermap.org/data/2.5/onecall?";
-    private static final String mc_TAG_API = "appid=";
-    private static final String mc_TAG_LATITUDE = "lat=";
-    private static final String mc_TAG_LONGITUDE = "lon=";
-    private static final String mc_TAG_UNITS = "units=";
-    private static final String mc_UNITS_IMPERIAL = "imperial"; // Fahrenheit,
-    private static final String mc_UNITS_METRIC = "metric"; // Celsius,
+    private static final String sfTAG = "OpenWeatherApi";
+
+    // Api Props
+    private static final String msfAPI_KEY =  "***REMOVED***";
+    private static final String msfONE_CALL_API_URL = "https://api.openweathermap.org/data/2.5/onecall?";
+    private static final String msfTAG_API = "appid=";
+    private static final String msfTAG_LATITUDE = "lat=";
+    private static final String msfTAG_LONGITUDE = "lon=";
+    private static final String msfTAG_UNITS = "units=";
+    private static final String msfUNITS_IMPERIAL = "imperial"; // Fahrenheit,
+    private static final String msfUNITS_METRIC = "metric"; // Celsius,
+
+    // JSON fields
+    private static final String msfFIELD_CURRENT = "current";
+    private static final String msfFIELD_MINUTELY = "minutely";
+    private static final String msfFIELD_HOURLY = "hourly";
+    private static final String msfFIELD_DAILY = "daily";
+    private static final String msfFIELD_TIMEZONE = "timezone";
+    private static final String msfFIELD_TIMEZONE_OFFSET = "timezone_offset";
+    private static final String msfFIELD_WEATHER = "weather";
+    private static final String msfFIELD_DESCRIPTION = "description";
+    private static final String msfFIELD_MAIN = "main";
+    private static final String msfFIELD_ICON = "icon";
+    private static final String msfFIELD_DT = "dt";
+    private static final String msfFIELD_SUNRISE = "sunrise";
+    private static final String msfFIELD_SUNSET = "sunset";
+    private static final String msfFIELD_TEMP = "temp";
+    private static final String msfFIELD_FEELS_LIKE = "feels_like";
+    private static final String msfFIELD_CLOUDS = "clouds";
+    private static final String msfFIELD_WIND_SPEED = "wind_speed";
+    private static final String msfFIELD_WIND_GUST = "wind_gust";
+    private static final String msfFIELD_RAIN = "rain";
+    private static final String msfFIELD_SNOW = "snow";
+    private static final String msfFIELD_1HR = "1hr";
+    private static final String msfFIELD_PRECIPITATION = "precipitation";
+    private static final String msfFIELD_DAY = "day";
+    private static final String msfFIELD_NIGHT = "night";
+    private static final String msfFIELD_EVE = "eve";
+    private static final String msfFIELD_MORN = "morn";
+    private static final String msfFIELD_MIN = "min";
+    private static final String msfFIELD_MAX = "max";
+
 
     // Constructor props
     private Context mContext; // Use getContext() or getActivity().getApplicationContext()
@@ -53,7 +85,7 @@ public class OpenWeatherApi {
     private long mCurrentUnixUtcTime;
 
     // Url props
-    private String mApiUrl = mc_ONE_CALL_API_URL;
+    private String mApiUrl = msfONE_CALL_API_URL;
     private int mNumberOfApiParams = 0;
 
     // Response JSON props
@@ -118,11 +150,11 @@ public class OpenWeatherApi {
     public void callApi(final ApiCallback apiCallback) {
 
         // Build Url
-        addParamToUrl(mc_TAG_LATITUDE + mLatitude.toString());
-        addParamToUrl(mc_TAG_LONGITUDE + mLongitude.toString());
-        addParamToUrl(mc_TAG_UNITS + mc_UNITS_IMPERIAL);
-        addParamToUrl(mc_TAG_API + mc_API_KEY);
-        Log.i(TAG, " - callApi Called. ApiUrl = \"" + mApiUrl + "\"");
+        addParamToUrl(msfTAG_LATITUDE + mLatitude.toString());
+        addParamToUrl(msfTAG_LONGITUDE + mLongitude.toString());
+        addParamToUrl(msfTAG_UNITS + msfUNITS_IMPERIAL);
+        addParamToUrl(msfTAG_API + msfAPI_KEY);
+        Log.i(sfTAG, " - callApi Called. ApiUrl = \"" + mApiUrl + "\"");
 
         // Build request
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
@@ -136,7 +168,7 @@ public class OpenWeatherApi {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, "callApi: There was an error with Volley's response.");
+                        Log.e(sfTAG, "callApi: There was an error with Volley's response.");
                     }
                 });
 
@@ -155,20 +187,15 @@ public class OpenWeatherApi {
     }
 
     private void parseResponseJson() {
-        Log.i(TAG, " - parseResponseJson Called.");
-        String currentTag = "current";
-        String minutelyTag = "minutely";
-        String hourlyTag = "hourly";
-        String dailyTag = "daily";
+        Log.i(sfTAG, " - parseResponseJson Called.");
 
         try {
-            mTimezoneString = mResponseJson.getString("timezone");
-            mTimezoneOffset_s = mResponseJson.getInt("timezone_offset");
-            mCurrentJson = mResponseJson.getJSONObject(currentTag);
-            mMinutelyJson = mResponseJson.getJSONArray(minutelyTag);
-            mHourlyJson = mResponseJson.getJSONArray(hourlyTag);
-            mDailyJson = mResponseJson.getJSONArray(dailyTag);
-            mTimezoneString = mResponseJson.getString("timezone");
+            mTimezoneString = mResponseJson.getString(msfFIELD_TIMEZONE);
+            mTimezoneOffset_s = mResponseJson.getInt(msfFIELD_TIMEZONE_OFFSET);
+            mCurrentJson = mResponseJson.getJSONObject(msfFIELD_CURRENT);
+            mMinutelyJson = mResponseJson.getJSONArray(msfFIELD_MINUTELY);
+            mHourlyJson = mResponseJson.getJSONArray(msfFIELD_HOURLY);
+            mDailyJson = mResponseJson.getJSONArray(msfFIELD_DAILY);
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -176,7 +203,7 @@ public class OpenWeatherApi {
     }
 
     public void parseResponse(JSONObject response) {
-        Log.i(TAG, " - parseResponse Called.");
+        Log.i(sfTAG, " - parseResponse Called.");
         mResponseJson = response;
         parseResponseJson();
         parseCurrent();
@@ -194,15 +221,15 @@ public class OpenWeatherApi {
     }
 
     private WeatherField parseWeatherFieldJson(JSONObject parentObjOfWeatherJsonArr) {
-        Log.i(TAG, " - parseWeatherJson Called.");
+        Log.i(sfTAG, " - parseWeatherJson Called.");
         WeatherField weatherField = new WeatherField();
         JSONArray weatherJsonArr;
         try {
-            weatherJsonArr = parentObjOfWeatherJsonArr.getJSONArray("weather");
+            weatherJsonArr = parentObjOfWeatherJsonArr.getJSONArray(msfFIELD_WEATHER);
             JSONObject weatherJsonObj = weatherJsonArr.getJSONObject(0);
-            weatherField.setDescription(weatherJsonObj.getString("description"));
-            weatherField.setMainDescriptionShort(weatherJsonObj.getString("main"));
-            weatherField.setIcon(weatherJsonObj.getString("icon"));
+            weatherField.setDescription(weatherJsonObj.getString(msfFIELD_DESCRIPTION));
+            weatherField.setMainDescriptionShort(weatherJsonObj.getString(msfFIELD_MAIN));
+            weatherField.setIcon(weatherJsonObj.getString(msfFIELD_ICON));
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -212,17 +239,17 @@ public class OpenWeatherApi {
     }
 
     private void parseCurrent() {
-        Log.i(TAG, " - ParseCurrent Called.");
+        Log.i(sfTAG, " - ParseCurrent Called.");
         mCurrentWeatherData = new CurrentWeatherData();
         try {
-            mCurrentWeatherData.setTime(Instant.ofEpochSecond(mCurrentJson.getLong("dt")));
-            mCurrentWeatherData.setSunriseTime(Instant.ofEpochSecond(mCurrentJson.getLong("sunrise")));
-            mCurrentWeatherData.setSunsetTime(Instant.ofEpochSecond(mCurrentJson.getLong("sunset")));
-            mCurrentWeatherData.setTemperature(mCurrentJson.getDouble("temp"));
-            mCurrentWeatherData.setTemperatureFeelsLike(mCurrentJson.getDouble("feels_like"));
-            mCurrentWeatherData.setPercentCloudy(mCurrentJson.getInt("clouds"));
+            mCurrentWeatherData.setTime(Instant.ofEpochSecond(mCurrentJson.getLong(msfFIELD_DT)));
+            mCurrentWeatherData.setSunriseTime(Instant.ofEpochSecond(mCurrentJson.getLong(msfFIELD_SUNRISE)));
+            mCurrentWeatherData.setSunsetTime(Instant.ofEpochSecond(mCurrentJson.getLong(msfFIELD_SUNSET)));
+            mCurrentWeatherData.setTemperature(mCurrentJson.getDouble(msfFIELD_TEMP));
+            mCurrentWeatherData.setTemperatureFeelsLike(mCurrentJson.getDouble(msfFIELD_FEELS_LIKE));
+            mCurrentWeatherData.setPercentCloudy(mCurrentJson.getInt(msfFIELD_CLOUDS));
             mCurrentWeatherData.setWeatherField(parseWeatherFieldJson(mCurrentJson));
-            mCurrentWeatherData.setWindSpeed(mCurrentJson.getDouble("wind_speed"));
+            mCurrentWeatherData.setWindSpeed(mCurrentJson.getDouble(msfFIELD_WIND_SPEED));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -230,36 +257,36 @@ public class OpenWeatherApi {
         // Set optional fields separately to avoid JSONException triggering the catch statement
         double windGust = 0.0;
         try {
-            windGust += mCurrentJson.getDouble("wind_gust");
+            windGust += mCurrentJson.getDouble(msfFIELD_WIND_GUST);
         } catch (JSONException ignored) {}
         mCurrentWeatherData.setWindGust(windGust);
 
         double precipitationVolume = 0.0;
         try {
-            JSONObject rain = mCurrentJson.getJSONObject("rain");
-            precipitationVolume += rain.getDouble("1hr");
+            JSONObject rain = mCurrentJson.getJSONObject(msfFIELD_RAIN);
+            precipitationVolume += rain.getDouble(msfFIELD_1HR);
         } catch (JSONException ignored) {}
         mCurrentWeatherData.setPrecipitationVolume(precipitationVolume);
 
         double snowVolume = 0.0;
         try {
-            JSONObject snow = mCurrentJson.getJSONObject("snow");
-            snowVolume += snow.getDouble("1hr");
+            JSONObject snow = mCurrentJson.getJSONObject(msfFIELD_SNOW);
+            snowVolume += snow.getDouble(msfFIELD_1HR);
         } catch (JSONException ignored) {}
         mCurrentWeatherData.setPrecipitationVolume(mCurrentWeatherData.getPrecipitationVolume() + snowVolume);
         mCurrentWeatherData.setSnowVolume(snowVolume);
     }
 
     private void parseMinutely() {
-        Log.i(TAG, " - parseMinutely Called.");
+        Log.i(sfTAG, " - parseMinutely Called.");
         mMinutelyWeatherData = new ArrayList<>();
         try {
             for (int iMinute = 0; iMinute < mMinutelyJson.length(); iMinute++) {
                 JSONObject minuteJson = mMinutelyJson.getJSONObject(iMinute);
                 MinutelyWeatherData minutelyWeatherData = new MinutelyWeatherData();
 
-                minutelyWeatherData.setTime(Instant.ofEpochSecond(minuteJson.getLong("dt")));
-                minutelyWeatherData.setPrecipitationVolume(minuteJson.getDouble("precipitation"));
+                minutelyWeatherData.setTime(Instant.ofEpochSecond(minuteJson.getLong(msfFIELD_DT)));
+                minutelyWeatherData.setPrecipitationVolume(minuteJson.getDouble(msfFIELD_PRECIPITATION));
 
                 mMinutelyWeatherData.add(minutelyWeatherData);
             }
@@ -271,38 +298,38 @@ public class OpenWeatherApi {
     }
 
     private void parseHourly() {
-        Log.i(TAG, " - parseHourly Called.");
+        Log.i(sfTAG, " - parseHourly Called.");
         mHourlyWeatherData = new ArrayList<>();
         try {
             for (int iHour = 0; iHour < mHourlyJson.length(); iHour++) {
                 JSONObject hourJson = mHourlyJson.getJSONObject(iHour);
                 HourlyWeatherData hourlyWeatherData = new HourlyWeatherData();
 
-                hourlyWeatherData.setTime(Instant.ofEpochSecond(hourJson.getLong("dt")));
-                hourlyWeatherData.setTemperature(hourJson.getDouble("temp"));
-                hourlyWeatherData.setTemperatureFeelsLike(hourJson.getDouble("feels_like"));
-                hourlyWeatherData.setPercentCloudy(hourJson.getInt("clouds"));
-                hourlyWeatherData.setWindSpeed(hourJson.getDouble("wind_speed"));
+                hourlyWeatherData.setTime(Instant.ofEpochSecond(hourJson.getLong(msfFIELD_DT)));
+                hourlyWeatherData.setTemperature(hourJson.getDouble(msfFIELD_TEMP));
+                hourlyWeatherData.setTemperatureFeelsLike(hourJson.getDouble(msfFIELD_FEELS_LIKE));
+                hourlyWeatherData.setPercentCloudy(hourJson.getInt(msfFIELD_CLOUDS));
+                hourlyWeatherData.setWindSpeed(hourJson.getDouble(msfFIELD_WIND_SPEED));
                 hourlyWeatherData.setWeatherField(parseWeatherFieldJson(hourJson));
 
                 // Set optional fields separately to avoid JSONException triggering the catch statement
                 double windGust = 0.0;
                 try {
-                    windGust += hourJson.getDouble("wind_gust");
+                    windGust += hourJson.getDouble(msfFIELD_WIND_GUST);
                 } catch (JSONException ignored) {}
                 hourlyWeatherData.setWindGust(windGust);
 
                 double precipitationVolume = 0.0;
                 try {
-                    JSONObject rain = hourJson.getJSONObject("rain");
-                    precipitationVolume += rain.getDouble("1hr");
+                    JSONObject rain = hourJson.getJSONObject(msfFIELD_RAIN);
+                    precipitationVolume += rain.getDouble(msfFIELD_1HR);
                 } catch (JSONException ignored) {}
                 hourlyWeatherData.setPrecipitationVolume(precipitationVolume);
 
                 double snowVolume = 0.0;
                 try {
-                    JSONObject snow = hourJson.getJSONObject("snow");
-                    snowVolume += snow.getDouble("1hr");
+                    JSONObject snow = hourJson.getJSONObject(msfFIELD_SNOW);
+                    snowVolume += snow.getDouble(msfFIELD_1HR);
                 } catch (JSONException ignored) {}
                 hourlyWeatherData.setPrecipitationVolume(hourlyWeatherData.getPrecipitationVolume() + snowVolume);
                 hourlyWeatherData.setSnowVolume(snowVolume);
@@ -315,36 +342,36 @@ public class OpenWeatherApi {
     }
 
     private void parseDaily() {
-        Log.i(TAG, " - parseDaily Called.");
+        Log.i(sfTAG, " - parseDaily Called.");
         mDailyWeatherData = new ArrayList<>();
         for (int iDay = 0; iDay < mDailyJson.length(); iDay++) {
             DailyWeatherData dailyWeatherData = new DailyWeatherData();
             try {
                 JSONObject dayJson = mDailyJson.getJSONObject(iDay);
-                dailyWeatherData.setTime(Instant.ofEpochSecond(dayJson.getLong("dt")));
-                dailyWeatherData.setSunriseTime(Instant.ofEpochSecond(dayJson.getLong("sunrise")));
-                dailyWeatherData.setSunriseTime(Instant.ofEpochSecond(dayJson.getLong("sunset")));
+                dailyWeatherData.setTime(Instant.ofEpochSecond(dayJson.getLong(msfFIELD_DT)));
+                dailyWeatherData.setSunriseTime(Instant.ofEpochSecond(dayJson.getLong(msfFIELD_SUNRISE)));
+                dailyWeatherData.setSunriseTime(Instant.ofEpochSecond(dayJson.getLong(msfFIELD_SUNSET)));
                 dailyWeatherData.setDailyTemp(parseDailyTemp(dayJson));
                 dailyWeatherData.setDailyTempFeelsLike(parseDailyTempFeelsLike(dayJson));
-                dailyWeatherData.setWindSpeed(dayJson.getDouble("wind_speed"));
+                dailyWeatherData.setWindSpeed(dayJson.getDouble(msfFIELD_WIND_SPEED));
                 dailyWeatherData.setWeatherField(parseWeatherFieldJson(dayJson));
-                dailyWeatherData.setPercentCloudy(dayJson.getInt("clouds"));
+                dailyWeatherData.setPercentCloudy(dayJson.getInt(msfFIELD_CLOUDS));
 
                 double windGust = 0.0;
                 try {
-                    windGust += dayJson.getDouble("wind_gust");
+                    windGust += dayJson.getDouble(msfFIELD_WIND_GUST);
                 } catch (JSONException ignored) {}
                 dailyWeatherData.setWindGust(windGust);
 
                 double precipitationVolume = 0.0;
                 try {
-                    precipitationVolume = dayJson.getDouble("rain");
+                    precipitationVolume = dayJson.getDouble(msfFIELD_RAIN);
                 } catch(JSONException ignored) {}
                 dailyWeatherData.setPrecipitationVolume(precipitationVolume);
 
                 double snowVolume = 0.0;
                 try {
-                    snowVolume = dayJson.getDouble("snow");
+                    snowVolume = dayJson.getDouble(msfFIELD_SNOW);
                 } catch(JSONException ignored) {}
                 dailyWeatherData.setPrecipitationVolume(dailyWeatherData.getPrecipitationVolume() + snowVolume);
                 dailyWeatherData.setSnowVolume(snowVolume);
@@ -361,13 +388,13 @@ public class OpenWeatherApi {
     private DailyTemp parseDailyTemp(JSONObject parentObjOfDailyTempJsonObj) {
         DailyTemp dailyTemp = new DailyTemp();
         try {
-            JSONObject dailyTempJsonObj = parentObjOfDailyTempJsonObj.getJSONObject("temp");
-            dailyTemp.setDay(dailyTempJsonObj.getDouble("day"));
-            dailyTemp.setNight(dailyTempJsonObj.getDouble("night"));
-            dailyTemp.setEvening(dailyTempJsonObj.getDouble("eve"));
-            dailyTemp.setMorning(dailyTempJsonObj.getDouble("morn"));
-            dailyTemp.setMax(dailyTempJsonObj.getDouble("min"));
-            dailyTemp.setMin(dailyTempJsonObj.getDouble("max"));
+            JSONObject dailyTempJsonObj = parentObjOfDailyTempJsonObj.getJSONObject(msfFIELD_TEMP);
+            dailyTemp.setDay(dailyTempJsonObj.getDouble(msfFIELD_DAY));
+            dailyTemp.setNight(dailyTempJsonObj.getDouble(msfFIELD_NIGHT));
+            dailyTemp.setEvening(dailyTempJsonObj.getDouble(msfFIELD_EVE));
+            dailyTemp.setMorning(dailyTempJsonObj.getDouble(msfFIELD_MORN));
+            dailyTemp.setMax(dailyTempJsonObj.getDouble(msfFIELD_MIN));
+            dailyTemp.setMin(dailyTempJsonObj.getDouble(msfFIELD_MAX));
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -378,11 +405,11 @@ public class OpenWeatherApi {
     private DailyTempFeelsLike parseDailyTempFeelsLike(JSONObject parentObjOfDailyTempFeelsLikeJsonObj) {
         DailyTempFeelsLike dailyTempFeelsLike = new DailyTempFeelsLike();
         try {
-            JSONObject dailyTempFeelsLikeJsonObj = parentObjOfDailyTempFeelsLikeJsonObj.getJSONObject("feels_like");
-            dailyTempFeelsLike.setDay(dailyTempFeelsLikeJsonObj.getDouble("day"));
-            dailyTempFeelsLike.setNight(dailyTempFeelsLikeJsonObj.getDouble("night"));
-            dailyTempFeelsLike.setEvening(dailyTempFeelsLikeJsonObj.getDouble("eve"));
-            dailyTempFeelsLike.setMorning(dailyTempFeelsLikeJsonObj.getDouble("morn"));
+            JSONObject dailyTempFeelsLikeJsonObj = parentObjOfDailyTempFeelsLikeJsonObj.getJSONObject(msfFIELD_FEELS_LIKE);
+            dailyTempFeelsLike.setDay(dailyTempFeelsLikeJsonObj.getDouble(msfFIELD_DAY));
+            dailyTempFeelsLike.setNight(dailyTempFeelsLikeJsonObj.getDouble(msfFIELD_NIGHT));
+            dailyTempFeelsLike.setEvening(dailyTempFeelsLikeJsonObj.getDouble(msfFIELD_EVE));
+            dailyTempFeelsLike.setMorning(dailyTempFeelsLikeJsonObj.getDouble(msfFIELD_MORN));
 
         } catch (JSONException e) {
             e.printStackTrace();
