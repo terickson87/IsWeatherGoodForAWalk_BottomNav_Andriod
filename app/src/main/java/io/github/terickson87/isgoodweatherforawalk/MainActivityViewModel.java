b42bd897037e8adff73743e722106e1e55669589
Todd Erickson <twericks@usc.edu>
2020-07-12 15:11:09 -0700

    Removed API Key.

--- app/src/main/java/io/github/terickson87/isgoodweatherforawalk/MainActivityViewModel.java
+++ app/src/main/java/io/github/terickson87/isgoodweatherforawalk/MainActivityViewModel.java
@@ -5,17 +5,21 @@ import android.content.Context;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.List;
+
 import io.github.terickson87.isgoodweatherforawalk.models.location.Location;
+import io.github.terickson87.isgoodweatherforawalk.models.weather.MinutelyWeatherData;
 import io.github.terickson87.isgoodweatherforawalk.models.weather.WeatherData;
 
 public class MainActivityViewModel extends ViewModel {
 
     private Repository mRepository;
     private MutableLiveData<Location> mLocation;
-    private MutableLiveData<WeatherData> mWeatherData;
+    private MutableLiveData<WeatherData> mWeatherData = new MutableLiveData<>();
     private MutableLiveData<Boolean> mIsUpdating = new MutableLiveData<>();
 
     public void init(Activity activity) {
--- app/src/main/java/io/github/terickson87/isgoodweatherforawalk/models/weather/OpenWeatherApi.java
+++ app/src/main/java/io/github/terickson87/isgoodweatherforawalk/models/weather/OpenWeatherApi.java
@@ -37,7 +37,7 @@ public class OpenWeatherApi {
     private static final String sfTAG = "OpenWeatherApi";
 
     // Api Props
-    private static final String msfAPI_KEY =  "***REMOVED***";
+    private static final String msfAPI_KEY =  "";
     private static final String msfONE_CALL_API_URL = "https://api.openweathermap.org/data/2.5/onecall?";
     private static final String msfTAG_API = "appid=";
     private static final String msfTAG_LATITUDE = "lat=";
